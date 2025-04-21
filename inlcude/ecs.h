21a19cacb0e16c4cdd82ae2b47724c37148e13e0
sukhi <sukhman.singh734@gmail.com>
2025-04-21 22:37:11 +0530

    added implementation of system class

--- inlcude/ecs.h
+++ inlcude/ecs.h
@@ -4,9 +4,19 @@
 #include <vector>
 const unsigned int MAX_COMPONENTS = 32;
 typedef std::bitset<MAX_COMPONENTS> Signature;
-class Component {
-  int id;
-  // TODO
+
+struct Base_component {
+protected:
+  static int next_id;
+};
+// used to assign unique id to per component type
+template <typename T> class Component : public Base_component {
+  // int id;
+  //  TODO
+  static int get_id() {
+    static int id = next_id++;
+    return id;
+  }
 };
 
 class Entity {
@@ -20,16 +30,23 @@ public:
 
 class System {
   Signature components_signature;
-std::vector<typename Tp>
-    // TODO
-    public : System() = default;
-  void add_entity_to_system();
-  void remove_entity_from_system();
+  std::vector<Entity> entities;
+  // TODO
+public:
+  System() = default;
+  void add_entity_to_system(Entity entity);
+  void remove_entity_from_system(Entity entity);
   std::vector<Entity> get_system_entities() const;
-  Signature &get_components_signature() const;
+  const Signature &get_components_signature() const;
+  template <typename Tcomponent> void require_comopent();
 };
 
 class Registry {
   // TODO
 };
+
+template <typename Tcomponent> void System::require_comopent() {
+  const int component_id = Component<Tcomponent>::get_id();
+  components_signature.set(component_id);
+}
 #endif // !ECS_H
--- src/ecs/ecs.cpp
+++ src/ecs/ecs.cpp
@@ -1,3 +1,24 @@
+#include <algorithm>
 #include <ecs.h>
+#include <vector>
 
 int Entity::get_id() const { return id; }
+
+void System::add_entity_to_system(Entity entity) {
+  entities.push_back(entity);
+  return;
+}
+
+void System::remove_entity_from_system(Entity entity) {
+  entities.erase(std::remove_if(entities.begin(), entities.end(),
+                                [&entity](Entity other) {
+                                  return other.get_id() == entity.get_id();
+                                }),
+                 entities.end());
+}
+
+std::vector<Entity> System::get_system_entities() const { return entities; }
+
+const Signature &System ::get_components_signature() const {
+  return components_signature;
+}
