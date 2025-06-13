1454593fc5b0f7b5ab663a9a87f5df1b57043753
sukhi <sukhman.singh734@gmail.com>
2025-06-13 23:16:28 +0530

    intergared sol lib for lua scripting

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,19 @@
 #include <game.hpp>
 #include <glm/vec3.hpp>
+#include <sol/sol.hpp>
 using namespace std;
 
 int main() {
+  sol::state lua;
+  int x = 0;
+  lua.set_function("beep", [&x] { ++x; });
+  lua.script("beep()");
+  assert(x == 1);
 
   Game game;
   game.init();
   game.run();
   game.destroy();
+
   return 0;
 }
